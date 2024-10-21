801492ce5b71256cb0d91d23ad5642d99f775607
Jundel Malazarte <Jundelmalazarte348@gmail.com>
2024-10-21 19:30:16 +0800

    updated index with --no-verify

--- index.js
+++ index.js
@@ -5,49 +5,39 @@ import random from "random";
 
 const path = "./data.json";
 
-const markCommit = (x, y) => {
+const markCommit = (x, y, callback) => {
   const date = moment()
     .subtract(1, "y")
     .add(1, "d")
     .add(x, "w")
     .add(y, "d")
-    .format();
+    .format(); // Generate random date
 
   const data = {
     date: date,
   };
 
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
+    // Commit with custom date
+    simpleGit()
+      .add([path])
+      .commit(date, { "--date": date, "--no-verify": true }, callback);
   });
 };
 
 const makeCommits = (n) => {
   if (n === 0) {
-    return simpleGit().push(); // Push after all commits are done
+    return simpleGit().push(); // Push after all commits
   }
 
-  const x = random.int(0, 54);  // Random week within a year (54 weeks)
-  const y = random.int(0, 6);   // Random day within a week (6 days)
-  const date = moment()
-    .subtract(1, "y")
-    .add(x, "w")
-    .add(y, "d")
-    .format();  // Generate random date
+  const x = random.int(0, 54); // Random week within a year (54 weeks)
+  const y = random.int(0, 6);  // Random day within a week (6 days)
 
-  const data = {
-    date: date,
-  };
-  
-  console.log(`Commit ${201 - n}: ${date}`);
-
-  jsonfile.writeFile(path, data, () => {
-    // Commit with a random date and recursively call to make more commits
-    simpleGit().add([path]).commit(date, { "--date": date }, () => {
-      makeCommits(n - 1);
-    });
+  markCommit(x, y, () => {
+    console.log(`Commit ${201 - n} made`);
+    makeCommits(n - 1);
   });
 };
 
-// Start making 200 commits
+// Start making 500 commits
 makeCommits(500);
