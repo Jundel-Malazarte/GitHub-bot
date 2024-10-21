a97dacf81400f8580dcb4e70f7fd0c6beb0850da
Jundel Malazarte <Jundelmalazarte348@gmail.com>
2024-10-22 00:14:37 +0800

    Initial update 2024 timestamp

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = "./data.json";
 const markCommit = async (x, y) => {
   // Adjust the date logic to focus on the year 2021
   const date = moment()
-    .year(2022) // Set year u want
+    .year(2024) // Set year u want
     .startOf('year') // Start from January 1st, 2021
     .add(x, "w") // Add random weeks
     .add(y, "d") // Add random days
@@ -41,10 +41,10 @@ const makeCommits = async (n) => {
   const y = random.int(0, 6);  // Random day within the week
 
   await markCommit(x, y);
-  console.log(`Commit ${51 - n} made with random date`); // Adjust the limit "Commit"
+  console.log(`Commit ${31 - n} made with random date`); // Adjust the limit "Commit"
 
   return makeCommits(n - 1); // Continue making commits
 };
 
-// Start making 50 commits
-makeCommits(50);
+// Start making commits -- set your commit
+makeCommits(30);
