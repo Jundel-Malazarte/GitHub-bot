dc23f589ab48028aeccc808ad30f8759177f0cd5
Jundel Malazarte <Jundelmalazarte348@gmail.com>
2024-10-21 19:23:24 +0800

    Some changes inside the index

--- index.js
+++ index.js
@@ -23,18 +23,31 @@ const markCommit = (x, y) => {
 };
 
 const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+  if (n === 0) {
+    return simpleGit().push(); // Push after all commits are done
+  }
+
+  const x = random.int(0, 54);  // Random week within a year (54 weeks)
+  const y = random.int(0, 6);   // Random day within a week (6 days)
+  const date = moment()
+    .subtract(1, "y")
+    .add(x, "w")
+    .add(y, "d")
+    .format();  // Generate random date
 
   const data = {
     date: date,
   };
-  console.log(date);
+  
+  console.log(`Commit ${201 - n}: ${date}`);
+
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+    // Commit with a random date and recursively call to make more commits
+    simpleGit().add([path]).commit(date, { "--date": date }, () => {
+      makeCommits(n - 1);
+    });
   });
 };
 
-makeCommits(100);
+// Start making 200 commits
+makeCommits(500);
