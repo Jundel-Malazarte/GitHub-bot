6483e276da66a46ba244bb0aaa64f73a93bdc505
Jundel Malazarte <Jundelmalazarte348@gmail.com>
2024-11-22 00:40:39 +0800

    Fixing the days behind

--- index.js
+++ index.js
@@ -40,11 +40,11 @@ const makeCommits = async (n, year, month, day) => {
 
   // Call markCommit with manually set year, month, and day
   await markCommit(year, month, day);
-  console.log(`Commit ${13 - n} made with date ${year}-${month}-${day}`); // Show commit number and date
+  console.log(`Commit ${35 - n} made with date ${year}-${month}-${day}`); // Show commit number and date
 
   return makeCommits(n - 1, year, month, day); // Continue making commits with the same date
 };
 
 // Start making commits -- set your commit year, month, and day
-makeCommits(12, 2024, 11, 11); // Example: making commits with the date Jan 1, 2024
+makeCommits(34, 2024, 11, 22); // Example: making commits with the date Jan 1, 2024
 // Commits, year, month, and day can be adjusted as needed.
